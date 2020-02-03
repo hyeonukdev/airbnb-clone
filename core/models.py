3be32b5cab16f324dd1a7b8b4842810d5b18ca84
hyeonukdev <lego7407@gmail.com>
2020-02-03 23:39:23 +0900

    finish models

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # 데이터베이스에 나타나지 않도록 함 abstract
+

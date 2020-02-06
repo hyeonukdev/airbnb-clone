d16a830dead4e209aaa45d14845ebd119adc507b
hyeonukdev <lego7407@gmail.com>
2020-02-07 01:39:29 +0900

    0207

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,73 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")}
+        ),
+        (
+            "Spaces",
+            {"fields": ("amenities", "facilities", "house_rules",)}
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),  # show hide
+                "fields": ("guests", "beds", "bedrooms", "baths",)
+            }
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        )
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ('name', 'price', "bedrooms")  # 정렬
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules"
+    )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "potato"
+
+    count_amenities.short_description = "change name"
 
 
 @admin.register(models.Photo)

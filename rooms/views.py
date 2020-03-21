fd18721642c6773c447d9cd987dc4552302a8d6d
hyeonukdev <lego7407@gmail.com>
2020-03-21 18:32:11 +0900

    0321_0

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,7 @@
 # from django.http import Http404
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -38,3 +39,9 @@ class RoomDetail(DetailView):
     """ RoomDetail Definition """
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
